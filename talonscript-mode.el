85053e7458e96bf53bda443579b7fe6a39e5c583
jcaw <40725916+jcaw@users.noreply.github.com>
2020-03-22 18:44:47 +0000

    Allow lists in left-hand side expressions
    Highlight the left-hand side even when it contains a list

--- talonscript-mode.el
+++ talonscript-mode.el
@@ -36,7 +36,7 @@
    ;; It's difficult to stop this highlighting left-hand expressions before the
    ;; divider (-). It's easier to let it highlight those with the same format,
    ;; so we tolerate variable syntax too.
-   (cons "^\\([(]?\\([]a-z-A-Z0-9[<> ._|)]\\|\\([^-a-zA-Z0-9_](\\)\\)*\\):"
+   (cons "^\\([(]?\\([]a-z-A-Z0-9[<>{} ._|)]\\|\\([^-a-zA-Z0-9_](\\)\\)*\\):"
          '(1 font-lock-function-name-face))
 
    ;; Matches variables in spoken form - doesn't match them elsewhere
